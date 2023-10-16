d1dd1085cdb073d23ff89b9c6562a7a5a7c20f71
David Dropping <ddropping@gmail.com>
2023-10-16 15:30:29 +0800

    feat: update footer text colors

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex justify-center align-middle bg-gray-900 text-white py-10">
-    <div class="text-2xl mr-20 flex flex-col justify-center items-center">
+  <div class="flex justify-center align-middle bg-gray-900 py-10">
+    <div
+      class="text-2xl mr-20 flex flex-col justify-center items-center text-white text-opacity-20 italic"
+    >
       <div>Be The Reason Someone</div>
-      <div>Smiles Today</div>
+      <div>Smiles Today...</div>
     </div>
 
-    <div class="ml-20 text-lg">
+    <div class="ml-20 text-lg text-white text-opacity-50">
       <ul>
         <li><a>github</a></li>
         <li><a>linkedIn</a></li>
