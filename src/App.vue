982908eab6d0e56696a57b7174426d3ef8b66cff
David Dropping <ddropping@gmail.com>
2023-10-16 10:17:23 +0800

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <Navbar />
-  <RouterView />
+  <div class="flex flex-col min-h-screen">
+    <Navbar />
+
+    <div class="grow">
+      <RouterView />
+    </div>
+
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Navbar from "./components/navbar/index.vue";
+import Footer from "./components/footer/index.vue";
 
 export default {
   name: "App",
-  components: { Navbar },
+  components: { Navbar, Footer },
   data() {},
 };
 </script>
--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,7 +1,22 @@
-<template lang="">
-  <div></div>
+<template>
+  <div class="flex justify-center align-middle bg-gray-900 text-white py-10">
+    <div class="text-2xl mr-20 flex flex-col justify-center items-center">
+      <div>Be The Reason Someone</div>
+      <div>Smiles Today</div>
+    </div>
+
+    <div class="ml-20 text-lg">
+      <ul>
+        <li><a>github</a></li>
+        <li><a>linkedIn</a></li>
+        <li><a>Email</a></li>
+      </ul>
+    </div>
+  </div>
 </template>
+
 <script>
 export default {};
 </script>
+
 <style lang=""></style>
