60a7da8ecda8a070be09304aa0040425c76f147e
David Dropping <ddropping@gmail.com>
2023-10-16 14:35:06 +0800

    chore: eslintrc

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["plugin:vue/vue3-recommended", "plugin:vue/recommended"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "vue/html-self-closing": "off",
+  },
+};
