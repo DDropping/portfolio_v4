cd41865cf375028909fe8f39518c806f9d25ff13
David Dropping <ddropping@gmail.com>
2023-10-16 09:53:13 +0800

    feat: navbar

---
+++ src/components/navbar/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="bg-gray-900">
+    <div class="flex justify-center p-4">
+      <ul class="flex">
+        <li class="ml-5 mr-5">
+          <RouterLink to="/" class="block py-2 pl-3 pr-4 text-white"
+            >Home</RouterLink
+          >
+        </li>
+        <li class="ml-5 mr-5">
+          <RouterLink to="/project" class="block py-2 pl-3 pr-4 text-white"
+            >Projects</RouterLink
+          >
+        </li>
+        <li class="ml-5 mr-5">
+          <RouterLink to="/about" class="block py-2 pl-3 pr-4 text-white"
+            >About</RouterLink
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
