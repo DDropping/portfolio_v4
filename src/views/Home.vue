dc26dd8bce0517a35993a5b791869feaa465706f
David Dropping <ddropping@gmail.com>
2023-10-16 09:54:28 +0800

    feat: home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>Home Page</div>
+  <AboutPreview />
+  <ProjectsCarousel />
 </template>
 
-<script></script>
+<script>
+import AboutPreview from "../components/aboutPreview/index.vue";
+import ProjectsCarousel from "../components/carousel/index.vue";
+
+export default {
+  name: "App",
+  components: { AboutPreview, ProjectsCarousel },
+  data() {},
+};
+</script>
 
 <style></style>
