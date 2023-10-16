339ed0a8bed5c65322df4c4d9577f4e8be4dd6f6
David Dropping <ddropping@gmail.com>
2023-10-16 14:35:22 +0800

    fix: footerComponent

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,17 @@
       <RouterView />
     </div>
 
-    <Footer />
+    <FooterComponent />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/navbar/index.vue";
-import Footer from "./components/footer/index.vue";
+import FooterComponent from "./components/footer/index.vue";
 
 export default {
   name: "App",
-  components: { Navbar, Footer },
+  components: { Navbar, FooterComponent },
   data() {},
 };
 </script>
