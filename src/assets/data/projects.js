320ceb2cd6c8a511bad96ef444290a70b64acae9
David Dropping <ddropping@gmail.com>
2023-10-19 18:52:59 +0900

    feat: projects data

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -9,65 +9,62 @@ export default [
   {
     name: "BoardRack v2",
     description:
-      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
+      "New/used surfboard marketplace that allows users to buy from, sell to and chat with others locally.",
     imgSrc: boardrackv2,
     websiteURL: "",
     githubURL: "",
+    technologies: ["React", "Redux", "Next.js", "Styled Components", "Vercel"],
   },
   {
     name: "BoardRack v1",
     description:
-      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
+      "New/used surfboard marketplace that allows users to buy from, sell to and chat with others locally.",
     imgSrc: boardRackv1,
     websiteURL: "",
     githubURL: "",
+    technologies: [
+      "React",
+      "Redux",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Sass",
+    ],
   },
   {
     name: "GatorRent",
     description:
-      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
+      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
     imgSrc: gatorrent,
     websiteURL: "",
     githubURL: "",
+    technologies: ["HTML", "CSS", "Node.js", "Express.js"],
   },
   {
     name: "Portfolio",
     description:
-      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
+      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
     imgSrc: portfoliov2,
     websiteURL: "",
     githubURL: "",
+    technologies: [],
   },
   {
     name: "SamsDesigns",
     description:
-      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
+      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
     imgSrc: samsdesign,
     websiteURL: "",
     githubURL: "",
+    technologies: ["React", "Gatsby"],
   },
   {
     name: "SortVis",
     description:
-      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
+      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
     imgSrc: sortvis,
     websiteURL: "",
     githubURL: "",
-  },
-  {
-    name: "SortVis",
-    description:
-      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
-    imgSrc: sortvis,
-    websiteURL: "",
-    githubURL: "",
-  },
-  {
-    name: "SortVis",
-    description:
-      "bskjd sdfjk sfk fsdlf kjsdf sdhfks fs fjsdkfj sdjf sls sk dfs dfj",
-    imgSrc: sortvis,
-    websiteURL: "",
-    githubURL: "",
+    technologies: ["React"],
   },
 ];
