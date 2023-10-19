d48545080627de901284b12b424bdb9aa3e77561
David Dropping <ddropping@gmail.com>
2023-10-20 08:23:18 +0900

    feat: responsive project cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="px-10">
     <AboutPreview />
-    <ProjectsCarousel />
+    <div>
+      <h2 class="text-center text-2xl mt-10">Recent Projects</h2>
+      <div class="hidden md:block">
+        <ProjectsCarousel />
+      </div>
+      <div class="block md:hidden">
+        <ProjectsCardStack />
+      </div>
+    </div>
     <SkillsList />
   </div>
 </template>
@@ -9,11 +17,12 @@
 <script>
 import AboutPreview from "../components/aboutPreview/index.vue";
 import ProjectsCarousel from "../components/carousel/index.vue";
+import ProjectsCardStack from "../components/cardStack/index.vue";
 import SkillsList from "../components/skills/index.vue";
 
 export default {
   name: "App",
-  components: { AboutPreview, ProjectsCarousel, SkillsList },
+  components: { AboutPreview, ProjectsCarousel, SkillsList, ProjectsCardStack },
   data() {},
 };
 </script>
