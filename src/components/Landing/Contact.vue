249cef28fb13aab51d898b5cee2e06615ac3d4f6
David Dropping <ddropping@gmail.com>
2023-10-30 15:03:02 -0700

    feat: contact component

--- src/components/Landing/Contact.vue
+++ src/components/Landing/Contact.vue
@@ -1,14 +1,62 @@
 <template>
-  <div :class="$style.containerd">
+  <div :class="$style.container">
     <TitleHeader title="Contact" />
+    <div :class="$style.content">
+      <div :class="$style.contactContainer">
+        <ContactForm />
+      </div>
+      <div :class="$style.socialsContainer">
+        <ul>
+          <li :class="$style.socialIcon">
+            <a
+              href="https://github.com/ddropping"
+              target="_blank"
+              rel="noopener"
+            >
+              <img :src="githubIcon" width="70" :class="$style.whiteIcon" />
+              /ddropping
+            </a>
+          </li>
+          <li :class="$style.socialIcon">
+            <a
+              href="https://www.linkedin.com/in/ddropping/"
+              target="_blank"
+              rel="noopener"
+            >
+              <img :src="linkedinIcon" width="70" :class="$style.whiteIcon" />
+              /ddropping
+            </a>
+          </li>
+          <li :class="$style.socialIcon">
+            <a href="mailto:ddropping@gmail.com" target="_blank" rel="noopener">
+              <img :src="emailIcon" width="70" :class="$style.whiteIcon" />
+              ddropping@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import TitleHeader from "./Header.vue";
+import ContactForm from "../contact/index.vue";
+import githubIcon from "../../assets/icons/github.png";
+import linkedinIcon from "../../assets/icons/linkedin.png";
+import emailIcon from "../../assets/icons/email.png";
+
 export default {
   components: {
     TitleHeader,
+    ContactForm,
+  },
+  setup() {
+    return {
+      githubIcon,
+      linkedinIcon,
+      emailIcon,
+    };
   },
 };
 </script>
@@ -18,8 +66,47 @@ export default {
   position: relative;
   height: 100vh;
   width: 100vw;
+}
+
+.content {
+  padding-top: 140px;
+  max-width: 1000px;
+  margin: 0 auto;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-around;
+}
+
+.contactContainer {
+  max-width: 500px;
+  width: 100%;
+  z-index: 10;
+}
+
+.socialsContainer {
+  max-width: 400px;
+  width: 100%;
+  z-index: 10;
+  font-size: 20px;
+}
+
+.socialIcon {
+  cursor: pointer;
+  margin-bottom: 40px;
+}
+
+.socialIcon > a > img {
+  display: inline-block;
+  filter: invert(96%) sepia(3%) saturate(575%) hue-rotate(184deg)
+    brightness(90%) contrast(89%);
+  transition-duration: 300ms;
+}
+
+.socialIcon:hover > a > img {
+  filter: invert(100%) sepia(20%) saturate(0%) hue-rotate(218deg)
+    brightness(104%) contrast(102%);
+}
+
+.socialIcon > a {
+  color: #6b7280;
 }
 </style>
--- src/components/contact/index.vue
+++ src/components/contact/index.vue
@@ -1,47 +1,44 @@
 <template>
-  <div>
-    <form @submit.prevent="handleSubmit">
-      <div class="mb-4">
-        <label for="name" class="text-gray-300 font-semibold">Name</label>
-        <input
-          v-model="name"
-          class="block rounded-md p-2 bg-transparent max-w-sm w-full ring-1 ring-gray-300 focus:ring-2 focus:border-none focus:outline-none duration-300"
-          placeholder="Your Name"
-        />
-      </div>
-      <div class="mb-4">
-        <label for="contact" class="text-gray-300 font-semibold">Contact</label>
-        <input
-          v-model="contactInfo"
-          class="block rounded-md p-2 bg-transparent max-w-sm w-full ring-1 ring-gray-300 focus:ring-2 focus:border-none focus:outline-none duration-300"
-          placeholder="Email or Phone Number"
-        />
-      </div>
-      <div class="mb-4">
-        <label for="name" class="text-gray-300 font-semibold">Message</label>
-        <textarea
-          v-model="message"
-          name="about"
-          rows="4"
-          placeholder="Write your message here..."
-          class="block rounded-md p-2 bg-transparent max-w-2xl w-full ring-1 ring-gray-300 focus:ring-2 focus:border-none focus:outline-none duration-300"
-        ></textarea>
-      </div>
+  <form @submit.prevent="handleSubmit" class="max-w-4xl w-full">
+    <div class="mb-4">
+      <label for="name" class="text-gray-500 font-semibold">Name</label>
+      <input
+        v-model="name"
+        class="block rounded-md p-2 bg-transparent max-w-sm w-full ring-1 ring-gray-500 focus:ring-2 focus:border-none focus:outline-none duration-300"
+        placeholder="Your Name"
+      />
+    </div>
+    <div class="mb-4">
+      <label for="contact" class="text-gray-500 font-semibold">Contact</label>
+      <input
+        v-model="contactInfo"
+        class="block rounded-md p-2 bg-transparent max-w-sm w-full ring-1 ring-gray-500 focus:ring-2 focus:border-none focus:outline-none duration-300"
+        placeholder="Email or Phone Number"
+      />
+    </div>
+    <div class="mb-4">
+      <label for="name" class="text-gray-500 font-semibold">Message</label>
+      <textarea
+        v-model="message"
+        name="about"
+        rows="4"
+        placeholder="Write your message here..."
+        class="block rounded-md p-2 bg-transparent max-w-2xl w-full ring-1 ring-gray-500 focus:ring-2 focus:border-none focus:outline-none duration-300"
+      ></textarea>
+    </div>
 
-      <button
-        type="submit"
-        class="bg-transparent text-gray-300 font-semibold py-2 px-8 border border-gray-300 hover:text-green-400 hover:border-green-400 rounded-md duration-300"
-      >
-        Send
-      </button>
-    </form>
-  </div>
+    <SlidingButton type="submit"> Send Message </SlidingButton>
+  </form>
 </template>
 
 <script>
 import { ref } from "vue";
+import SlidingButton from "../SlidingButton.vue";
 
 export default {
+  components: {
+    SlidingButton,
+  },
   setup() {
     const name = ref("");
     const contactInfo = ref("");
