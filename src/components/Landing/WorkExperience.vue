a4b88ed2e7dd00d25e383079c41585b9f5360cef
David Dropping <ddropping@gmail.com>
2023-11-03 17:07:07 -1000

    feat: add header to work experience

--- src/components/Landing/WorkExperience.vue
+++ src/components/Landing/WorkExperience.vue
@@ -1,9 +1,16 @@
 <template>
-  <div></div>
+  <section :class="$style.container">
+    <div class="overflow-x-hidden">
+      <TitleHeader id="workExperienceSection" title="My Journey" />
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import TitleHeader from "./Header.vue";
+export default {
+  components: { TitleHeader },
+};
 </script>
 
 <style></style>
