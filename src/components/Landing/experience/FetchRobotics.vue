3a079b52bf6f9f503e5cdbeecff6e71236ac410f
David Dropping <ddropping@gmail.com>
2023-11-04 13:49:59 -1000

    feat: setup experience section

---
+++ src/components/Landing/experience/FetchRobotics.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Fetch Robotics</h2>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
---
+++ src/components/Landing/experience/ZebraTechnologies.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Zebra Technologies</h2>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
