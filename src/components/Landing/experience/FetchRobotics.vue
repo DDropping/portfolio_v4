d2548767151618ecf94587ae8378a78799555d06
David Dropping <ddropping@gmail.com>
2023-11-05 07:40:32 -1000

    feat: job summaries

--- src/components/Landing/experience/FetchRobotics.vue
+++ src/components/Landing/experience/FetchRobotics.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h2>Fetch Robotics</h2>
+    <p>
+      Provider of on-demand Autonomous Mobile Robots for the warehousing and
+      intralogistics markets.
+    </p>
+    <p>Software Engineer</p>
   </div>
 </template>
 <script>
--- src/components/Landing/experience/ZebraTechnologies.vue
+++ src/components/Landing/experience/ZebraTechnologies.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h2>Zebra Technologies</h2>
+    <p>
+      A global company specializing in AIDC (Automatic Identification and Data
+      Capture) solutions, including barcoding, RFID technology, mobile
+      computing, and software, to help businesses enhance their operational
+      efficiency and asset management across various industries.
+    </p>
+    <p>Software Engineer</p>
   </div>
 </template>
 <script>
