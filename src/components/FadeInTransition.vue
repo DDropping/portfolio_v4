119c63fc9b1cf6647ee784756b2e039a7cd05493
David Dropping <ddropping@gmail.com>
2023-11-07 11:44:21 -1000

    feat: fade in transition component

---
+++ src/components/FadeInTransition.vue
@@ -0,0 +1,27 @@
+<template l>
+  <div
+    v-motion
+    :initial="{
+      opacity: 0,
+      transition: {
+        stiffness: 100,
+      },
+    }"
+    :visible="{
+      opacity: 1,
+      transition: {
+        stiffness: 100,
+        ease: 'easeInOut',
+      },
+    }"
+    :delay="200"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
